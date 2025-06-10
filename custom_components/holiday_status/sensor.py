c3bbd5db9d84109487f68391e9827dedbf410606
jmal <zhushilun084@gmail.com>
2025-06-10 16:15:03 +0800

    init

--- custom_components/holiday_status/sensor.py
+++ custom_components/holiday_status/sensor.py
@@ -19,10 +19,10 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
     """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
+    add_entities([HolidayStatus()])
 
 
-class ExampleSensor(SensorEntity):
+class HolidayStatus(SensorEntity):
     """Representation of a Sensor."""
 
     _attr_name = "Example Temperature"
